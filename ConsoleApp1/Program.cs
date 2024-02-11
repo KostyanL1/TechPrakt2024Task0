0a8d8102d441eeaf8448d49224dcc3745cbf3aaa
Kostyantin <kostyantim.l05@gmail.com>
2024-02-11 13:03:00 +0200

    add all files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: ");
+            string input1 = Console.ReadLine();
+            Console.Write("Введіть друге число: ");
+            string input2 = Console.ReadLine();
+            if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+            {
+                double sum = number1 + number2;
+                Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Введені значення не є числами.");
+            }
+        }
+    }
+}
